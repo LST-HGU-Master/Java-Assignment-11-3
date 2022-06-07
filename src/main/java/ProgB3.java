7f81caf9d6367a0a814417cd0b968ad05c1af15f
YoshiHirat <50605381+YoshiHirat@users.noreply.github.com>
2022-06-07 16:35:51 +0900

    Update ProgB3.java

--- src/main/java/ProgB3.java
+++ src/main/java/ProgB3.java
@@ -1,25 +1,25 @@
 public class ProgB3
 {
-	public static void main(String arg[]){
-		//３つのFarmerクラスのインスタンスの宣言と代入；
-		Farmer fm1 = new Farmer("農民「山田」", 20);
-		Farmer fm2 = new Farmer("農民「山本」", 28);
-		Farmer fm3 = new Farmer("農民「山本」", 20);
-		Farmer fm4 = new Farmer("農民「山本」", 28);
-		// toStringメソッドを使う
-		System.out.println(fm1.toString());
-		System.out.println(fm2.toString());
-		System.out.println(fm3.toString());
-		System.out.println(fm4.toString());
-		// equalsメソッドを使う
-		System.out.print("fm2とfm1は");
-		if(fm2.equals(fm1)) System.out.println("同じ人です。");
-		else                System.out.println("違う人です。");
-		System.out.print("fm2とfm3は");
-		if(fm2.equals(fm3)) System.out.println("同じ人です。");
-		else                System.out.println("違う人です。");
-		System.out.print("fm2とfm4は");
-		if(fm2.equals(fm4)) System.out.println("同じ人です。");
-		else                System.out.println("違う人です。");
-	}
+    public static void main(String arg[]){
+        //３つのFarmerクラスのインスタンスの宣言と代入；
+        Farmer fm1 = new Farmer("農民「山田」", 20);
+        Farmer fm2 = new Farmer("農民「山本」", 28);
+        Farmer fm3 = new Farmer("農民「山本」", 20);
+        Farmer fm4 = new Farmer("農民「山本」", 28);
+        // toStringメソッドを使う
+        System.out.println(fm1.toString());
+        System.out.println(fm2.toString());
+        System.out.println(fm3.toString());
+        System.out.println(fm4.toString());
+        // equalsメソッドを使う
+        System.out.print("fm2とfm1は");
+        if(fm2.equals(fm1)) System.out.println("同じ人です。");
+        else                System.out.println("違う人です。");
+        System.out.print("fm2とfm3は");
+        if(fm2.equals(fm3)) System.out.println("同じ人です。");
+        else                System.out.println("違う人です。");
+        System.out.print("fm2とfm4は");
+        if(fm2.equals(fm4)) System.out.println("同じ人です。");
+        else                System.out.println("違う人です。");
+    }
 }
