055aeb2b292f371c14af5aaa3e040ee282394f2b
YoshiHirat <50605381+YoshiHirat@users.noreply.github.com>
2022-06-10 17:05:02 +0900

    Update FarmerTest.java

--- src/test/java/FarmerTest.java
+++ src/test/java/FarmerTest.java
@@ -1,7 +1,9 @@
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import org.junit.jupiter.api.Test;
 import java.io.*;
-
+/**
+ * @version (20220610)
+ */
 public class FarmerTest {
 
     @Test
@@ -11,8 +13,9 @@ public class FarmerTest {
         Farmer fm2 = new Farmer("農民「小林」", 80);
 
         // assertion
-        assertEquals("Name：農民「佐々木」/HP:20", fm1.toString());
-        assertEquals("Name：農民「小林」/HP:80", fm2.toString());
+        String msg = "オーバーライドしたtoString()の出力が実行例と異なります!";
+        assertEquals("Name：農民「佐々木」/HP:20", fm1.toString(), msg);
+        assertEquals("Name：農民「小林」/HP:80", fm2.toString(), msg);
     }
 
     @Test
@@ -23,8 +26,10 @@ public class FarmerTest {
         Farmer fm3 = new Farmer("農民「小林」", 80);
 
         // assertion
-        assertEquals(false, fm1.equals(fm2));
-        assertEquals(false, fm1.equals(fm3));
-        assertEquals(true, fm2.equals(fm3));
+        String msg = "オーバーライドしたequals()の出力が正しくありません!";
+        assertEquals(true, fm1.equals(fm1), msg);
+        assertEquals(false, fm1.equals(fm2), msg);
+        assertEquals(false, fm1.equals(fm3), msg);
+        assertEquals(true, fm2.equals(fm3), msg);
     }
 }
